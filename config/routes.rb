850f5b04b4eb397dbdc4d91dbb84fc80d7aa80f0
Drishtdyumn <drishtdyumn.shrivastava@go-yubi.com>
2023-02-20 18:03:11 +0530

    add user user migration and model

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
     root 'pages#landingpage'
     get 'aboutus', to:'pages#aboutus'
-    resources :articles, only: [:show, :index, :new, :create]
+    resources :articles
+    resources :users
 
   # Defines the root path route ("/")
   # root "articles#index"
