3fee7f909fbdd3f739fe884196a0b684b9efcba9
Drishtdyumn <drishtdyumn.shrivastava@go-yubi.com>
2023-02-21 00:51:15 +0530

    add secure password using salt and BCrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
+    before_save {self.email = email.downcase}
     has_many :articles 
     validates :username , presence: true , length: {minimum: 4, maximum: 30}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email , presence: true , length: { maximum: 80}, format: { with: VALID_EMAIL_REGEX} 
+
+    has_secure_password
 end
